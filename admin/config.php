47f0211569e90c988cd67e47e4111e51b0288933
shadow <alexangel1975@gmail.com>
2019-03-13 14:56:22 +0300

    ''

---
+++ admin/config.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://petshop.loc/admin/');
+define('HTTP_CATALOG', 'http://petshop.loc/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://petshop.loc/admin/');
+define('HTTPS_CATALOG', 'http://petshop.loc/');
+
+// DIR
+define('DIR_APPLICATION', 'C:/OSPanel/domains/petshop.loc/admin/');
+define('DIR_SYSTEM', 'C:/OSPanel/domains/petshop.loc/system/');
+define('DIR_IMAGE', 'C:/OSPanel/domains/petshop.loc/image/');
+define('DIR_LANGUAGE', 'C:/OSPanel/domains/petshop.loc/admin/language/');
+define('DIR_TEMPLATE', 'C:/OSPanel/domains/petshop.loc/admin/view/template/');
+define('DIR_CONFIG', 'C:/OSPanel/domains/petshop.loc/system/config/');
+define('DIR_CACHE', 'C:/OSPanel/domains/petshop.loc/system/storage/cache/');
+define('DIR_DOWNLOAD', 'C:/OSPanel/domains/petshop.loc/system/storage/download/');
+define('DIR_LOGS', 'C:/OSPanel/domains/petshop.loc/system/storage/logs/');
+define('DIR_MODIFICATION', 'C:/OSPanel/domains/petshop.loc/system/storage/modification/');
+define('DIR_UPLOAD', 'C:/OSPanel/domains/petshop.loc/system/storage/upload/');
+define('DIR_CATALOG', 'C:/OSPanel/domains/petshop.loc/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'petshop');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
---
+++ config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://petshop.loc/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://petshop.loc/');
+
+// DIR
+define('DIR_APPLICATION', 'C:/OSPanel/domains/petshop.loc/catalog/');
+define('DIR_SYSTEM', 'C:/OSPanel/domains/petshop.loc/system/');
+define('DIR_IMAGE', 'C:/OSPanel/domains/petshop.loc/image/');
+define('DIR_LANGUAGE', 'C:/OSPanel/domains/petshop.loc/catalog/language/');
+define('DIR_TEMPLATE', 'C:/OSPanel/domains/petshop.loc/catalog/view/theme/');
+define('DIR_CONFIG', 'C:/OSPanel/domains/petshop.loc/system/config/');
+define('DIR_CACHE', 'C:/OSPanel/domains/petshop.loc/system/storage/cache/');
+define('DIR_DOWNLOAD', 'C:/OSPanel/domains/petshop.loc/system/storage/download/');
+define('DIR_LOGS', 'C:/OSPanel/domains/petshop.loc/system/storage/logs/');
+define('DIR_MODIFICATION', 'C:/OSPanel/domains/petshop.loc/system/storage/modification/');
+define('DIR_UPLOAD', 'C:/OSPanel/domains/petshop.loc/system/storage/upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'petshop');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
